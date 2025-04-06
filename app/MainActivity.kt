48d5613b1e4c0684e2482e80654afd5d1fb147d5
Mohamed Fazil Z <65252590+fazil2003@users.noreply.github.com>
2025-04-06 21:07:02 +0530

    Create MainActivity.kt

---
+++ app/MainActivity.kt
@@ -0,0 +1,7 @@
+@Composable
+fun SkillTwinHome() {
+    Text("Welcome to AI Skilling Twin")
+    Button(onClick = { /* trigger voice or recommend */ }) {
+        Text("Start Skill Match")
+    }
+}
